962eda1d57bb9c6bd7c6fed5d3f177fb926eea9f
Jannatul Ferdous Lam <127182865+jannatsoftit@users.noreply.github.com>
2023-04-03 16:12:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
     }
 
     $students = $objcrudpro->display_data();
+
+    if(isset($_GET['status'])){
+        if($_GET['status'] = 'delete'){
+            $delete_id = $_GET['id'];
+            $delmsg =  $objcrudpro->delete_data($delete_id);
+        }
+    }
 ?>
 
 
@@ -16,7 +23,7 @@
 <html lang="en">
 <head>
 
-    <!-- Mera Tags-->
+    <!-- Meta Tags-->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,8 +37,9 @@
 <body>
 
     <div class="container my-4 p-4 shadow"> 
+
         <h2><a style="text-decoration: none;" href="index.php">Student Information</a></h2>
-        
+            <?php if(isset($delmsg)){ echo $delmsg; } ?>
         <form class="form" action="" method="post" enctype="multipart/form-data">
             <?php if(isset($return_msg)) {echo $return_msg;} ?>
 
@@ -54,6 +62,7 @@
                         <th>Action</th>
                     </tr> 
                 </thead>
+
                 <tbody>
 
                 <?php while($student = mysqli_fetch_assoc($students)){ ?>
@@ -64,7 +73,7 @@
                         <td><img style="height:100px;" src="upload/<?php echo $student['std_img'];?>"></td>
                         <td>
                             <a class="btn btn-success" href="edit.php?status=edit&&id=<?php echo $student['id']; ?>">Edit</a>
-                            <a class="btn btn-warning" href="#">Delete</a>
+                            <a class="btn btn-warning" href="?status=delete&&id=<?php echo $student['id']; ?>">Delete</a>
                         </td> 
                     </tr> 
 
